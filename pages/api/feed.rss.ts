fd7eaab4ac7638730a6767772c4611b8d201ab78
Victor Vila Ases <irikuo@gmail.com>
2020-04-07 21:17:16 +0200

    Improved RSS feed

--- pages/api/feed.rss.ts
+++ pages/api/feed.rss.ts
@@ -1,17 +1,29 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { NextApiRequest, NextApiResponse } from 'next';
 import RSS from 'rss';
+import { getPosts } from '../../utils/posts';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default async (_: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Content-type', 'application/rss+xml');
   res.setHeader('Cache-Control', 'maxage=0, s-maxage=600');
 
-  res.send(
-    new RSS({
-      title: 'Relateando',
-      feed_url: 'https://relateando.es/api/feed.rss',
-      site_url: 'https://relateando.es/',
-    }).xml(),
-  );
+  const feed = new RSS({
+    title: 'Relateando',
+    feed_url: 'https://relateando.es/api/feed.rss',
+    site_url: 'https://relateando.es/',
+    language: 'es',
+  });
+
+  getPosts().map((post) => {
+    feed.item({
+      title: post.title,
+      description: post.summary,
+      url: `https://relateando.es/${post.slug}`,
+      author: post.author,
+      date: post.date,
+    });
+  });
+
+  res.send(feed.xml());
 };
